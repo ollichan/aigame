1d7373e91e4e923944c94303d9981bcaf738493d
shunnokw <33548195+shunnokw@users.noreply.github.com>
2017-11-12 14:00:28 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
-//this game will have only 1 state
-var GameState = {
-    //load the game assets before the game starts
-    perload: function(){
-        this.load.image('background','assets/images/background.jpg')
-    },
-    //executed after everything is loaded
-    create: function () {
-        this.background = this.game.add.sprite(0,0, 'background');
-    },
-    //this is executed multiple times per second
-    update: function () {
+var game = new Phaser.Game(1290, 720, Phaser.CANVAS,'',{preload: preload, create: create, update: update});
 
-    }
-};
+function preload() {
+    game.load.image('background','assets/images/background.jpg')
+}
 
-//initiate the Phaser framework
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
+function create() {
+    game.add.sprite(0,0, 'background');
+    game.physics.startSystem(Phaser.Physics.ARCADE);
 
-game.state.add('GameState', GameState);
-game.state.start('GameState');
+    //player
+    player = game.add.sprite(playerX, playerY, 'spritesheet', 'character/solider/walkLeft/0001');
+    player.scale.setTo(2,2);
+    player.anchor.x=0.5;
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+    player.body.collideWorldBounds = true;
+    player.body.bounce.set(0.2);
+    player.body.allowGravity = false;
+    player.animations.add('left', Phaser.Animation.generateFrameNames('character/solider/walkLeft/', 1, 6, '', 4), 10, true, false);
+    player.animations.add('right', Phaser.Animation.generateFrameNames('character/solider/walkRight/', 1, 6, '', 4), 10, true, false);
+    isAttackingAnimLeft = player.animations.add('aleft', Phaser.Animation.generateFrameNames('character/solider/attackLeft/', 1, 3, '', 4), 10, true, false);
+    isAttackingAnimRight = player.animations.add('aright', Phaser.Animation.generateFrameNames('character/solider/attackRight/', 1, 3, '', 4), 10, true, false);
+    isDieAnimLeft = player.animations.add('dleft', Phaser.Animation.generateFrameNames('character/solider/dieLeft/', 5, 5, '', 4), 10, true, false);
+    isDieAnimRight = player.animations.add('dright', Phaser.Animation.generateFrameNames('character/solider/dieRight/', 5, 5, '', 4), 10, true, false);
+    isAttackingAnimLeft.onStart.add(attackLeftAnimationStarted, this);
+    isAttackingAnimLeft.onLoop.add(attackLeftAnimationLooped, this);
+    isAttackingAnimLeft.onComplete.add(attackLeftStop, this);
+    isAttackingAnimRight.onStart.add(attackRightAnimationStarted, this);
+    isAttackingAnimRight.onLoop.add(attackRightAnimationLooped, this);
+    isAttackingAnimRight.onComplete.add(attackRightStop, this);
+    isDieAnimLeft.onStart.add(dieLeftAnimationStarted,this)
+    isDieAnimLeft.onLoop.add(dieLeftAnimationLooped,this);
+    isDieAnimLeft.onComplete.add(dieLeftStop,this);
+    isDieAnimRight.onStart.add(dieRightAnimationStarted,this)
+    isDieAnimRight.onLoop.add(dieRightAnimationLooped,this);
+    isDieAnimRight.onComplete.add(dieRightStop,this);
+
+}
+
+function update(){
+
+}
+
+
