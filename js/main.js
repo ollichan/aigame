9a391ada8cf3e945a256c95b54b609b3510e7d0c
shunnokw <33548195+shunnokw@users.noreply.github.com>
2017-11-19 04:01:46 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,44 +1,127 @@
-var game = new Phaser.Game(1290, 720, Phaser.CANVAS,'',{preload: preload, create: create, update: update});
+var game = new Phaser.Game(960, 540, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.image('background','assets/images/background.jpg')
+    game.load.image('background','assets/images/background.jpg');
+    game.load.atlas('spritesheet','assets/images/spritesheet.png','assets/images/spritesheet.json');
 }
+
+var player;
+var playerX=285,playerY=285;
+var cursors;
+var BGM=new Audio("assets/audio/LLOP2.mp3");
+BGM.load();
 
 function create() {
     game.add.sprite(0,0, 'background');
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
+    //BGM
+    BGM.play();
+    BGM.loop = true;
+
+    // animation
+    cursors = game.input.keyboard.createCursorKeys();
+
+    startScreen();
+
+    resize();
     //player
-    player = game.add.sprite(playerX, playerY, 'spritesheet', 'character/solider/walkLeft/0001');
-    player.scale.setTo(2,2);
+    player = game.add.sprite(playerX, playerY, 'spritesheet', 'sprites/stealer/stand/stealer_stand_left_1.png');
+    player.scale.setTo(0.1,0.1);
     player.anchor.x=0.5;
     game.physics.enable(player, Phaser.Physics.ARCADE);
     player.body.collideWorldBounds = true;
     player.body.bounce.set(0.2);
     player.body.allowGravity = false;
-    player.animations.add('left', Phaser.Animation.generateFrameNames('character/solider/walkLeft/', 1, 6, '', 4), 10, true, false);
-    player.animations.add('right', Phaser.Animation.generateFrameNames('character/solider/walkRight/', 1, 6, '', 4), 10, true, false);
-    isAttackingAnimLeft = player.animations.add('aleft', Phaser.Animation.generateFrameNames('character/solider/attackLeft/', 1, 3, '', 4), 10, true, false);
-    isAttackingAnimRight = player.animations.add('aright', Phaser.Animation.generateFrameNames('character/solider/attackRight/', 1, 3, '', 4), 10, true, false);
-    isDieAnimLeft = player.animations.add('dleft', Phaser.Animation.generateFrameNames('character/solider/dieLeft/', 5, 5, '', 4), 10, true, false);
-    isDieAnimRight = player.animations.add('dright', Phaser.Animation.generateFrameNames('character/solider/dieRight/', 5, 5, '', 4), 10, true, false);
-    isAttackingAnimLeft.onStart.add(attackLeftAnimationStarted, this);
-    isAttackingAnimLeft.onLoop.add(attackLeftAnimationLooped, this);
-    isAttackingAnimLeft.onComplete.add(attackLeftStop, this);
-    isAttackingAnimRight.onStart.add(attackRightAnimationStarted, this);
-    isAttackingAnimRight.onLoop.add(attackRightAnimationLooped, this);
-    isAttackingAnimRight.onComplete.add(attackRightStop, this);
-    isDieAnimLeft.onStart.add(dieLeftAnimationStarted,this)
-    isDieAnimLeft.onLoop.add(dieLeftAnimationLooped,this);
-    isDieAnimLeft.onComplete.add(dieLeftStop,this);
-    isDieAnimRight.onStart.add(dieRightAnimationStarted,this)
-    isDieAnimRight.onLoop.add(dieRightAnimationLooped,this);
-    isDieAnimRight.onComplete.add(dieRightStop,this);
+    player.animations.add('left', Phaser.Animation.generateFrameNames('sprites/stealer/walk_left/', 1, 8, '', 4), 10, true, false);
+    player.animations.add('right', Phaser.Animation.generateFrameNames('sprites/stealer/walk_left/', 1, 8,'', 4), 10, true, false);
 
 }
+
+var playerIsLeftDirection=false;
+var moverSpeed = 150;
+var creationMutex = false;
 
 function update(){
-
+    playerControll();
 }
 
+function resize(){
+    widthRatio = game.width / 960;
+    heightRatio = game.height / 540;
+    game.stage.backgroundColor = "#FFFFFF";
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignVertically = true;
+    game.scale.setScreenSize( true );
+}
 
+function playerControll(){
+    player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+
+    if(cursors.up.isDown&&cursors.left.isDown){
+        player.body.velocity.x = -moverSpeed;
+        player.body.velocity.y = -moverSpeed;
+        playerIsLeftDirection = true;
+        player.animations.play('left',false);
+
+    }else if(cursors.down.isDown&&cursors.left.isDown){
+        player.body.velocity.x = -moverSpeed;
+        player.body.velocity.y = moverSpeed;
+        playerIsLeftDirection = true;
+        player.animations.play('left',false);
+    }else if(cursors.up.isDown&&cursors.right.isDown){
+        player.body.velocity.x = moverSpeed;
+        player.body.velocity.y = -moverSpeed;
+        playerIsLeftDirection = false;
+        player.animations.play('right',false);
+    }else if(cursors.down.isDown&&cursors.right.isDown){
+        player.body.velocity.x = moverSpeed;
+        player.body.velocity.y = moverSpeed;
+        playerIsLeftDirection = false;
+        player.animations.play('right',false);
+    }else if(cursors.up.isDown){
+        player.body.velocity.y = -moverSpeed;
+        if(playerIsLeftDirection)
+            player.animations.play('left',false);
+        else
+            player.animations.play('right',false);
+
+    }
+    else if(cursors.down.isDown){
+        player.body.velocity.y = moverSpeed;
+        if(playerIsLeftDirection)
+            player.animations.play('left',false);
+        else
+            player.animations.play('right',false);
+
+    }
+    else if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -moverSpeed;
+        playerIsLeftDirection = true;
+        player.animations.play('left',false);
+
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = moverSpeed;
+        playerIsLeftDirection = false;
+        player.animations.play('right',false);
+
+    }
+    else
+    {
+        player.animations.stop();
+        player.frame = 0;
+    }
+}
+
+function startScreen(){
+    instructionText = game.add.text(game.world.centerX+240,game.world.centerY-20,' ', { font: '38px Arial', fill: '#FFF' ,boundsAlignH: "left", boundsAlignV: "middle"});
+    instructionText.anchor.setTo(0.5, 0.5);
+    instructionText.visible = true;
+    instructionText.text = "Welcome to God Steal\nPress arrow key to move\n";
+    instructionText.stroke = '#6d6d6d';
+    instructionText.strokeThickness = 4;
+}
